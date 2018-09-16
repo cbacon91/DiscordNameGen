38db54a7a3f34fb32b50323aad2156b7bdf98364
Chris Bacon <cbacon91@gmail.com>
2018-09-16 14:49:33 -0600

    name command tests completed

--- tests/nameCommand.spec.ts
+++ tests/nameCommand.spec.ts
@@ -4,6 +4,7 @@ import { NameArgsParser } from '../src/commands/namecommand/nameArgsParser';
 import { NameRepository } from '../src/commands/namecommand/nameRepository';
 import { NameArgs } from '../src/commands/namecommand/nameArgs';
 import { TextChannel, Message } from 'discord.js';
+import { GeneratedNames } from '../src/commands/namecommand/generatedNames';
 
 describe('nameCommand', () => {
   let command: NameCommand;
@@ -17,27 +18,78 @@ describe('nameCommand', () => {
     channel = jasmine.createSpyObj('channel', ['send']);
     logger = jasmine.createSpyObj('logger', ['log']);
     parser = jasmine.createSpyObj('parser', ['parseArgs']);
-    repo = jasmine.createSpyObj('repo', ['get']);
+    repo = jasmine.createSpyObj('repo', ['getNamesAsync']);
+
 
     command = new NameCommand(logger, parser, repo);
     message = jasmine.createSpyObj('message', ['channel']);
-    message.channel.and.returnValue(channel);
+    (<any>message.channel) = channel;
   });
 
   describe('run', () => {
     it('should return early if args error', async () => {
       const errMsg = 'oops';
-      console.log('iit');
-      // parser.parseArgs.and.returnValue(new NameArgs({
-      //   error: [errMsg]
-      // }));
-      // const message = {
-      //   channel: {
-          
-      //   }
-      // }
+      parser.parseArgs.and.returnValue(new NameArgs({
+        error: [errMsg]
+      }));
 
+      await command.run(message, 'arg');
 
+      expect(channel.send).toHaveBeenCalledWith([`**${errMsg}**`]);
+      expect(repo.getNamesAsync).not.toHaveBeenCalled();
+    });
+
+    it('should return early if generated error', async () => {
+      const errMsg = 'oops';
+      parser.parseArgs.and.returnValue(new NameArgs());
+      repo.getNamesAsync.and.returnValue(new GeneratedNames({
+        error: [errMsg]
+      }));
+
+      await command.run(message, 'arg');
+
+      expect(channel.send).toHaveBeenCalledWith([`**${errMsg}**`]);
+      expect(repo.getNamesAsync).toHaveBeenCalled();
+    });
+
+    it('should return include args messages', async () => {
+      const msg = 'random';
+      parser.parseArgs.and.returnValue(new NameArgs({
+        message: [msg]
+      }));
+      repo.getNamesAsync.and.returnValue(new GeneratedNames({}));
+
+      await command.run(message, 'arg');
+
+      expect(channel.send).toHaveBeenCalledWith([`*${msg}*`, '']);
+      expect(repo.getNamesAsync).toHaveBeenCalled();
+    });
+
+    it('should return include generated messages', async () => {
+      const msg = 'random';
+      parser.parseArgs.and.returnValue(new NameArgs({
+      }));
+      repo.getNamesAsync.and.returnValue(new GeneratedNames({
+        message: [msg]
+      }));
+
+      await command.run(message, 'arg');
+
+      expect(channel.send).toHaveBeenCalledWith([`*${msg}*`, '']);
+      expect(repo.getNamesAsync).toHaveBeenCalled();
+    });
+
+    it('should return include names', async () => {
+      const name = 'tom foolery';
+      parser.parseArgs.and.returnValue(new NameArgs({}));
+      repo.getNamesAsync.and.returnValue(new GeneratedNames({
+        names: [name]
+      }));
+
+      await command.run(message, 'arg');
+
+      expect(channel.send).toHaveBeenCalledWith(['',name]);
+      expect(repo.getNamesAsync).toHaveBeenCalled();
     });
   });
 
